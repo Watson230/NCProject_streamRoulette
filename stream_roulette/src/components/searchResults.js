7cf166cb9439b0344aef8068facbc2fe51e31390
Dave <dave@Nicks-MBP-2.home>
2018-04-17 19:56:50 +0100

    fetch req implemented to get URLs of selected film from Utelly Api

--- stream_roulette/src/components/searchResults.js
+++ stream_roulette/src/components/searchResults.js
@@ -11,13 +11,13 @@ class SearchResults extends Component {
         currentFilm: [],
         filmNumber: 4,
         likedFilms: [],
-        PickFilmflag:false,
-        recentlyDisliked:[]
+        PickFilmflag: false,
+        recentlyDisliked: []
     }
 
     componentDidMount() {
 
-        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b714d4feb8707f01b7dd25f75051d8a6`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=b714d4feb8707f01b7dd25f75051d8a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.lte=2015`)
             .then(res => {
                 console.log(res)
                 return res.json();
@@ -26,12 +26,12 @@ class SearchResults extends Component {
 
                 this.setState({
 
-                    searchResults: body.results.slice(2, 9),
-                    currentFilm: body.results[3],
+                    searchResults: body.results.slice(0, 30),
+                    currentFilm: body.results[0],
                     likedFilms: [],
-                    PickFilmflag:false,
-                    selectedFilm:[],
-                    recentlyDisliked:this.state.recentlyDisliked
+                    PickFilmflag: false,
+                    selectedFilm: [],
+                    recentlyDisliked: this.state.recentlyDisliked
 
 
                 })
@@ -54,46 +54,73 @@ class SearchResults extends Component {
             searchResults: this.state.searchResults.slice(1, this.state.searchResults.length),
             currentFilm: this.state.searchResults[0],
             likedFilms: this.state.likedFilms.concat(film),
-            PickFilmflag:false,
-            selectedFilm:[],
-            recentlyDisliked:this.state.recentlyDisliked
+            PickFilmflag: false,
+            selectedFilm: [],
+            recentlyDisliked: this.state.recentlyDisliked
         })
     }
 
     disLikeFilmhandler = () => {
 
-        let film= this.state.currentFilm
+        let film = this.state.currentFilm
 
         this.setState({
-           
+
             searchResults: this.state.searchResults.slice(1, this.state.searchResults.length),
             currentFilm: this.state.searchResults[0],
             likedFilms: this.state.likedFilms,
-            PickFilmflag:false,
-            selectedFilm:[],
-            recentlyDisliked:film
+            PickFilmflag: false,
+            selectedFilm: [],
+            recentlyDisliked: film
         })
 
     }
 
-    pickFilmhandler =()=>{
+    pickFilmhandler = () => {
 
         this.setState({
-            
+
             searchResults: this.state.searchResults,
             currentFilm: this.state.currentFilm,
-            PickFilmflag:true,
+            PickFilmflag: true,
             likedFilms: this.state.likedFilms,
-            selectedFilm:this.state.likedFilms[Math.floor(Math.random() * this.state.likedFilms.length)  ],
-            recentlyDisliked:this.state.recentlyDisliked
+            selectedFilm: this.state.likedFilms[Math.floor(Math.random() * this.state.likedFilms.length)],
+            recentlyDisliked: this.state.recentlyDisliked
 
         })
+
+    }
+
+    getPickedFilmURLS = () => {
+        console.log(this.state.selectedFilm.title)
+        fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.mashape.com/lookup?term=${this.state.selectedFilm.title}`,
+            {
+
+
+                headers: new Headers({
+                    'Accept': 'application/json',
+                    'X-Mashape-Key': "lbLD5PKDXhmshZXMNJgHsm1ahtF2p1fYk5Sjsn8XmYhTzIKQYn"
+                }),
+                type: 'cors'
+
+            })
+            .then(res => {
+                console.log(res)
+                return res.json();
+            })
+            .then(body => {
+                console.log(body)
+            })
+            .catch(err => {
+                console.log(err)
+
+            })
 
 
     }
 
     render() {
-        console.log(this.state)
+
         return (
             <div>
                 <NavBar />
@@ -143,23 +170,23 @@ class SearchResults extends Component {
                     <div style={{ "float": "left", "margin-left": "100px", "width": "400px" }}>
                         <h1 class="title is-1">Are you sure?</h1>
                         <div class="box">
-                           
-                        <div class="content">
-                                                   <figure class="image is-4by5">
-                                                       <img src={`http://image.tmdb.org/t/p/w185//${this.state.recentlyDisliked.poster_path}`} alt="Image" />
-                                                   </figure>
-                   
-                                               </div>
+
+                            <div class="content">
+                                <figure class="image is-4by5">
+                                    <img src={`http://image.tmdb.org/t/p/w185//${this.state.recentlyDisliked.poster_path}`} alt="Image" />
+                                </figure>
+
+                            </div>
                         </div>
 
                         <div>
 
                             <h1 class="title is-2">{`Liked Films:${this.state.likedFilms.length}`}</h1>
 
-                            <button class="button is-link" onClick={()=>{
+                            <button class="button is-link" onClick={() => {
                                 this.pickFilmhandler()
                             }}
-                            
+
                             > Pick a Film!</button>
 
                         </div>
@@ -167,8 +194,8 @@ class SearchResults extends Component {
                     </div>
                 </div>
                 {
-                this.state.PickFilmflag > 0 ?
-                
+                    this.state.PickFilmflag > 0 ?
+
                         <div class="modal is-active">
                             <div class="modal-background"></div>
                             <div class="modal-card">
@@ -180,23 +207,25 @@ class SearchResults extends Component {
 
                                 <section class="modal-card-body">
                                     {
-                                                   <div class="content">
-                                                   <figure class="image is-4by5">
-                                                       <img src={`http://image.tmdb.org/t/p/w185//${this.state.selectedFilm.poster_path}`} alt="Image" />
-                                                   </figure>
-                   
-                                               </div>
+                                        <div class="content">
+                                            <figure class="image is-4by5">
+                                                <img src={`http://image.tmdb.org/t/p/w185//${this.state.selectedFilm.poster_path}`} alt="Image" />
+                                            </figure>
+
+                                        </div>
 
                                     }
                                 </section>
 
                                 <footer class="modal-card-foot">
                                     <button class="button is-success"
-                                        
+                                        onClick={() => {
+                                            this.getPickedFilmURLS()
+                                        }}
 
                                     >Watch</button>
                                     <button class="button"
-                                     
+
 
                                     >Cancel</button>
                                 </footer>
@@ -205,7 +234,7 @@ class SearchResults extends Component {
                         </div> : <div></div>
                 }
             </div>
-            
+
 
 
         )
